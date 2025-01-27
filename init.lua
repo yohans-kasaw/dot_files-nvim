ea7dd1ac250c7768ab6d243a59429b025feb4848
yohans <emailyyohans@gmail.com>
2025-01-27 16:48:56 +0300

    refactor(nvim): Enhance CMP configuration and streamline core settings
    BREAKING CHANGE: Major refactoring of completion system
    
    - Reorganizes CMP setup with improved structure and readability
    - Consolidates spell checking and auto-commands
    - Removes redundant options and cleans up init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
----call plug#end()
-
--- used for complition
-vim.api.nvim_set_keymap("n", "<A-d>", "", { noremap = true, silent = true })
-
 Icons = require("user.icons")
 
 require("user.options")
@@ -13,47 +8,5 @@ require("user.cmp")
 require("user.nvim_tree")
 require("user.keymaps")
 require("user.telescope")
-vim.opt.signcolumn = "yes"
-vim.g.airline_theme = "oceanicnext"
-vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
-vim.opt.smartcase = true
-
-local function enable_zen_mode_for_note_files()
-	local file_ext = vim.fn.expand("%:e")
-	if file_ext == "note" then
-		require("zen-mode").toggle()
-	end
-end
-
-vim.api.nvim_create_autocmd("BufRead", {
-	pattern = "*.note",
-	callback = enable_zen_mode_for_note_files,
-})
-
-vim.api.nvim_create_autocmd("BufNewFile", {
-	pattern = "*.note",
-	callback = enable_zen_mode_for_note_files,
-})
 
 vim.cmd("colorscheme kanagawa")
-vim.opt.relativenumber = true
-
--- Initialize spell checking for Markdown and text files
-vim.api.nvim_create_autocmd({"FileType"}, {
-    pattern = {"markdown", "text"},
-    callback = function()
-        vim.opt_local.spell = true
-        vim.opt_local.spelllang = "en_us" -- Change to your preferred language(s)
-    end,
-})
-
--- Enable spell checking for files with no detected filetype
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-    callback = function()
-        if vim.bo.filetype == "" then
-            vim.opt_local.spell = true
-            vim.opt_local.spelllang = "en_us" -- Change to your preferred language(s)
-        end
-    end,
-})
-
--- lua/user/cmp/init.lua
+++ lua/user/cmp/init.lua
@@ -1,122 +1,126 @@
-local cmp = require("cmp")
-local lspkind = require("lspkind")
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require('cmp')
+local cmp_mapping = require('cmp.config.mapping')
 
-local cmp_mapping = require("cmp.config.mapping")
+-- Local helper functions
 local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
-local combined_formatter = function(entry, vim_item)
-	-- Apply lspkind formatting
-	vim_item = lspkind.cmp_format({
-		mode = "symbol",
-		before = function(entry, vim_item)
-			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				buffer = "[Buffer]",
-				path = "[Path]",
-				-- cmdline = "[Cmd]",
-			})[entry.source.name]
-			return vim_item
-		end,
-	})(entry, vim_item)
+-- Shared configurations
+local matching_settings = {
+  disallow_fuzzy_matching = false,
+  disallow_partial_matching = false,
+  disallow_prefix_unmatching = false,
+  disallow_fullfuzzy_matching = false,
+  disallow_partial_fuzzy_matching = false,
+  disallow_symbol_nonprefix_matching = false,
+}
 
-	-- Apply Tailwind colorizer formatting
-	vim_item = tailwind_formatter(entry, vim_item)
+local completion_settings = {
+  ignore_case = true,
+}
 
-	return vim_item
-end
+local source_menu_icons = {
+  nvim_lsp = '[LSP]',
+  buffer = '[Buffer]',
+  path = '[Path]',
+  cmdline = '[Cmd]',
+}
 
-cmp.setup({
-	window = {
-		completion = cmp.config.window.bordered({
-			winhighlight = "Normal:CmpCompletionNormal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-		}),
-		documentation = cmp.config.window.bordered({
-			winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-		}),
-	},
-
-	formatting = {
-		format = require("lspkind").cmp_format({
-			mode = "symbol",
-			before = function(entry, vim_item)
-				-- Incorporate tailwind-tools formatting
-				vim_item = require("tailwind-tools.cmp").lspkind_format(entry, vim_item)
-
-				-- Add menu labels
-				vim_item.menu = ({
-					nvim_lsp = "[LSP]",
-					buffer = "[Buffer]",
-					path = "[Path]",
-					-- cmdline = "[Cmd]",
-				})[entry.source.name]
-
-				return vim_item
-			end,
-		}),
-	},
-
-	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp_mapping(function(fallback)
-			if cmp.visible() and has_words_before() then
-				cmp.select_next_item()
-			elseif has_words_before() then
-				fallback()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp_mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<C-Space>"] = cmp_mapping.complete(),
-		["<C-e>"] = cmp_mapping.abort(),
-		["<CR>"] = cmp_mapping(function(fallback)
-			if cmp.visible() and cmp.get_selected_entry() then
-				cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-
-	sources = cmp.config.sources(
-		{ { name = "nvim_lsp" } },
-		{ { name = "buffer" } },
-		{ { name = "path" } },
-		-- {{ name = 'cmdline' },},
-		{ { name = "lazydev", group_index = 0 } },
-		{ { name = "treesitter" } }
-		-- {{name = 'supermaven'}}
-		-- { { name = "copilot" } }
-	),
+-- Window configuration
+local bordered_completion = cmp.config.window.bordered({
+  winhighlight = 'Normal:CmpCompletionNormal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
 })
 
--- cmp.setup.cmdline({ '/', '?' }, {
---   mapping = cmp.mapping.preset.cmdline(),
---   sources = {
---     { name = 'buffer' }
---   }
--- })
---
--- cmp.setup.cmdline(':', {
---   mapping = cmp.mapping.preset.cmdline(),
---   sources = cmp.config.sources({
---     { name = 'path' }
---   }, {
---     -- { name = 'cmdline' }
---   }),
---   matching = { disallow_symbol_nonprefix_matching = false }
--- })
+local bordered_documentation = cmp.config.window.bordered({
+  winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
+})
 
--- cmp.event:on(
---   'confirm_done',
---   cmp_autopairs.on_confirm_done()
--- )
+-- Mapping helpers
+local fallback = function(fb) fb() end
+
+local handle_tab = function(fallback_fn)
+  if cmp.visible() and has_words_before() then
+    cmp.select_next_item()
+  else
+    fallback_fn()
+  end
+end
+
+local handle_shift_tab = function(fallback_fn)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  else
+    fallback_fn()
+  end
+end
+
+local confirm_selection = function(fallback_fn)
+  if cmp.visible() and cmp.get_selected_entry() then
+    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+  else
+    fallback_fn()
+  end
+end
+
+-- Main cmp setup
+cmp.setup({
+  window = {
+    completion = bordered_completion,
+    documentation = bordered_documentation,
+  },
+
+  formatting = {
+    fields = { 'abbr', 'kind', 'menu' },
+    expandable_indicator = true,
+    format = require('lspkind').cmp_format({
+      mode = 'symbol_text',
+      before = function(entry, vim_item)
+        vim_item = require('tailwind-tools.cmp').lspkind_format(entry, vim_item)
+        vim_item.menu = source_menu_icons[entry.source.name] or vim_item.menu
+        return vim_item
+      end,
+    }),
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_mapping(handle_tab, { 'i', 's' }),
+    ['<S-Tab>'] = cmp_mapping(handle_shift_tab, { 'i', 's' }),
+    ['<C-Space>'] = cmp_mapping.complete(),
+    ['<C-e>'] = cmp_mapping.abort(),
+    ['<CR>'] = cmp_mapping(confirm_selection, { 'i', 's' }),
+  }),
+
+  matching = matching_settings,
+  completion = completion_settings,
+
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'cmdline' },
+    { name = 'lazydev', group_index = 0 },
+    { name = 'treesitter' },
+  }),
+})
+
+-- Cmdline configurations
+local cmdline_mapping = cmp.mapping.preset.cmdline()
+
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmdline_mapping,
+  matching = matching_settings,
+  completion = completion_settings,
+  sources = { { name = 'buffer' } },
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmdline_mapping,
+  sources = cmp.config.sources(
+    { { name = 'path' } },
+    { { name = 'cmdline' } }
+  ),
+  matching = matching_settings,
+  completion = completion_settings,
+})
